427e54011cd6a554409da1ed0d53864872707401
Stephen Denham <Stephen.d3nham@gmail.com>
2023-04-16 13:12:50 -0400

    haha ai

--- Scream Jam 2022+1/Assets/Antlogic.cs
+++ Scream Jam 2022+1/Assets/Antlogic.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Antlogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int AntDifficulty = 5;
+    public float timeLeft = 30.0f;
+    public int Antpositon = 1;
+    public int randomNumber;
+    public GameObject Antposition1;
+    public GameObject Antposition2;
+    public GameObject Antposition3;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            int randomnumber = Random.Range(0, 20);
+            if (randomNumber <= AntDifficulty)
+            {
+                Antpositon = Antpositon + 1;
+                timeLeft = 30;
+            }
+        }
+        if (Antpositon == 1)
+        {
+            Antposition2.SetActive(true);
+            Antposition1.SetActive(false);
+        }
+        if (Antpositon == 0)
+        {
+            Antposition2.SetActive(false);
+            Antposition1.SetActive(true);
+        }
+        if (Antpositon >= 2)
+        {
+            Antposition2.SetActive(false);
+            Antposition1.SetActive(false);
+            Antposition3.SetActive(true);
+        }
+
     }
 }
